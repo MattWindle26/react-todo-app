88059a045e152cbd84992ff76a9c05fbe8af6c78
MattWindle26 <matthewwindle@outlook.com>
2021-02-20 23:25:02 +0000

    adding all files for react todo app

---
+++ src/Components/TodoList.js
@@ -0,0 +1,24 @@
+
+import Todo from "./Todo"
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+
+    return(
+        <div className="todo-containr">
+            <ul className="todo-list">
+
+                {filteredTodos.map(todo => (
+                    <Todo 
+                        setTodos={setTodos}
+                        todos={todos}
+                        key={todo.id}
+                        text={todo.text}
+                        todo={todo}
+                     />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
