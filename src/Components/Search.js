2cfaaa04a3782703b5ee7e7024ca7a346e9757fa
Matt Windle <matthewwindle@outlook.com>
2021-02-25 20:50:24 +0000

    Fixed pointer event issue on form

---
+++ src/Components/Search.js
@@ -0,0 +1,24 @@
+
+const Search = (
+    setStatus,
+) => {
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value)
+    }
+
+    return (
+        <form action="">
+            <div className="select">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+
+    );
+}
+ 
+export default Search;
